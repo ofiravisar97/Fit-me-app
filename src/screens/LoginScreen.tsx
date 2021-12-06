aed433f8d7b68c9787fe3f6a19193831c287a581
ofir <ofiravisar97gmail.com>
2021-12-06 08:47:43 +0200

    second init

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -11,7 +11,22 @@ const LoginScreen = () => {
   }
 
   const styles = StyleSheet.create({
-    container: {flex: 1, alignItems: 'center', justifyContent: 'center',backgroundColor: STYLES.light_2_color}
+    container: {flex: 1, alignItems: 'center', justifyContent: 'center',backgroundColor: STYLES.light_2_color,
+padding: 1},
+
   });
+
+
+  /**
+   * 1. Title
+   * 2. 2 Inputs
+   * 3. forgot password
+   * 4.Login Button
+   * 5.dosent have account ?
+   * 6. Facebook/google
+   * 7.Loading bar
+   * 
+   * 
+   */
 
   export default LoginScreen;
